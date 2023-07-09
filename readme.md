9975577d1913aa442d1ae372978118ac4c4ab8b7
JusticeWheelwright <justice792@outlook.com>
2023-07-09 13:07:25 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Overview
+
+This program was written as basically a personal crash course for the c# language. I have made tic tac toe programs before but I have never before used c#. So I approached this program which I was familiar with in an entirely unknown language and added a special twist which is that the computer will be playing against the player instead of another player.
+
+This program contains a basic tic tac toe program created in c# utilizing the console as the display and interaction method. It gives the user the ability to play tic tac toe against a computer opponent following all the rules of the game allowing the user to make moves and compete against a computer player.
+
+[Software Demo Video](https://www.youtube.com/watch?v=3s9FkiQWIvM)
+
+# Development Environment
+
+This program was developed in visual studio community edition 2022. It does not utilize outside libraries. It is run from the Program.cs file in the tic_tac_toe folder.
+
+# Useful Websites
+
+- [Basic documentation](https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/)
+- [c# tutorials (I looked at many of them)](https://www.w3schools.com/cs/index.php)
+
+# Future Work
+
+- Clean up visual display and make the output cleaner
---
+++ tic_tac_toe/Program.cs
@@ -0,0 +1,137 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    static string[] board = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    static string player = "x";
+    static int move;
+    static bool end = false;
+    static void Main(string[] args)
+    {
+        while (end==false)
+        {
+            Console.WriteLine("\nit is {0} turn", player);
+            draw();
+            if (player == "x")
+            {
+                player = player_move();
+            }
+            else
+            {
+                player = computer_move();
+            }
+            end = check_end(board);
+        }
+        Console.WriteLine("\nPlayer {0} loses", player);
+        draw();
+    }
+    static void draw()
+    {
+        Console.WriteLine(" {0} | {1} | {2} ", board[0], board[1], board[2]);
+        Console.WriteLine("------------");
+        Console.WriteLine(" {0} | {1} | {2} ", board[3], board[4], board[5]);
+        Console.WriteLine("------------");
+        Console.WriteLine(" {0} | {1} | {2} ", board[6], board[7], board[8]);
+    }
+    static string player_move()
+    {
+        Console.WriteLine("\nWhat spot would you like to take?");
+        move = Int32.Parse(Console.ReadLine());
+        while (board[move-1] != move.ToString())
+        {
+            Console.WriteLine("\nThat spot is not open. What spot would you like to take?");
+            move = Int32.Parse(Console.ReadLine());
+        }
+        board[move-1] = player;
+        return "o";
+    }
+
+    static string computer_move()
+    {
+        bool cont = true;
+        for (int a=0; a<=8;  a++)
+        {
+            if (board[a] == (a + 1).ToString())
+            {
+                string[] board_check = (string[]) board.Clone();
+                board_check[a] = "x";
+                bool win = check_end(board_check);
+                if (win)
+                {
+                    move = a;
+                    cont = false;
+                    break;
+                }
+                board_check[a] = "o";
+                win = check_end(board_check);
+                if (win)
+                {
+                    move = a;
+                    cont=false;
+                    break;
+                }
+            }
+        }
+        if (cont)
+        {
+            Random rnd = new Random();
+            int sample = rnd.Next(0,8);
+            while (board[sample]!= (sample + 1).ToString())
+            {
+                rnd = new Random();
+                sample = rnd.Next(0, 8);
+            }
+            move = sample;
+        }
+        board[move] = player;
+        Console.WriteLine("The computer took spot {0}", (move+1));
+        return "x";
+    }
+    static bool check_end(string[] board)
+    {
+        if (board[0] == board[1] && board[0] == board[2])
+        {
+            return true;
+        }
+        if (board[3] == board[4] && board[3] == board[5])
+        {
+            return true;
+        }
+        if (board[6] == board[7] && board[6] == board[8])
+        {
+            return true;
+        }
+        if (board[0] == board[3] && board[0] == board[6])
+        {
+            return true;
+        }
+        if (board[1] == board[4] && board[1] == board[7])
+        {
+            return true;
+        }
+        if (board[2] == board[5] && board[2] == board[8])
+        {
+            return true;
+        }
+        if (board[0] == board[4] && board[0] == board[8])
+        {
+            return true;
+        }
+        if (board[6] == board[4] && board[6] == board[2])
+        {
+            return true;
+        }
+        for (int i=0; i<=8; i++)
+        {
+            if (board[i] == (i + 1).ToString())
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+}
+
